e09a448fb9aa86024d7461ad41c33d9151419fb8
FrisoDev <frisodevstoker@gmail.com>
2017-11-06 11:05:50 +0100

    tings

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
-// index.js
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const passport = require('./config/auth')
-const { recipes, users, sessions } = require('./routes')
+const http = require('http')
+const socketAuth = require('./config/socket-auth')
+const socketIO = require('socket.io')
 
 const port = process.env.PORT || 3030
 
-let app = express()
+const app = express()
+const server = http.Server(app)
+const io = socketIO(server)
 
-app
-  .use(cors())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(bodyParser.json())
-  .use(passport.initialize())
+// using auth middleware
+io.use(socketAuth);
 
-  .use(recipes)
-  .use(users)
-  .use(sessions)
-
-  // catch 404 and forward to error handler
-  .use((req, res, next) => {
-    const err = new Error('Not Found')
-    err.status = 404
-    next(err)
-  })
-
-  // final error handler
-  .use((err, req, res, next) => {
-    res.status(err.status || 500)
-    res.send({
-      message: err.message,
-      error: app.get('env') === 'development' ? err : {}
-    })
-  })
-
-  .listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
-  })
+io.on('connect', socket => {
+  socket.emit('ping', `Welcome to the server, ${socket.request.user.name}`)
+  console.log(`${socket.request.user.name} connected to the server`)
+})
